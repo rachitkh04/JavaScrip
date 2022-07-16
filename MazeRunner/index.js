053e7e45c693661d9afe4250ce8c74a97d80bcf7
Rachit <rachitkh04@gmail.com>
2022-07-16 22:38:55 +0530

    new commit

--- MazeRunner/index.js
+++ MazeRunner/index.js
@@ -1,47 +1,50 @@
 //Boilerplate code
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
+const cells = 4;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        wireframes: false,
+        wireframes: true,
         width,
         height
     }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine)
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}));
 
 //Creating walls 
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-    Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 
 // adding walls to the world
 World.add(world, walls)
 
-//creating Random Shapes
-for (let i = 0; i < 20; i++) {
-    if (Math.random() > 0.5) {
-        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, { isStatic: false }));
-    }
-    else {
-        World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-            render: {
-                fillStyle: "green" //Filling all circle with green colour
-            }
-        }));
+//Maze Generation
+const grid = Array(cells)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
 
-    }
-}
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+console.log(horizontals);
